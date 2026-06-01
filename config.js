db9340f37f88932f886f1f65dc7bf8fb251d598e
Benjamin Pasquariello <admin@pasqai.com>
2026-06-01 11:59:36 +0200

    Add Azure client ID

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-  clientId: "YOUR_AZURE_CLIENT_ID",
+  clientId: "2bf71f20-78aa-471e-85bf-fcddae392b2f",
   baseUrl: "https://pasqai-technologies.github.io/outlook-contact-saver",
   excelFileName: "OutlookContacts.xlsx",
   excelSheetName: "Contacts",
